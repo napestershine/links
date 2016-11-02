0961791d9349248ee2028c7ee8eeba110b75172a
root <napestershine@gmail.com>
2016-11-03 01:47:33 +0530

    passed first test by entering manual data by caling factory in seeder test file

--- tests/unit/SeederTest.php
+++ tests/unit/SeederTest.php
@@ -8,6 +8,9 @@ class SeederTest extends TestCase
 {
     public function testLinksTable()
     {
+        factory(App\Link::class)->create([
+            'title' => 'dotdev.co'
+        ]);
         $this->seeInDatabase('links', ['title' => 'dotdev.co']);
     }
 }
