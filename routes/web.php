f1c8d6a46ea4bb6b567f0cd1e59356e5a8d2aee7
root <napestershine@gmail.com>
2016-11-03 08:28:50 +0530

    added route and showing error

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     $links = \App\Link::all();
     return view('welcome', compact('links'));
@@ -22,4 +24,16 @@ Route::get('/home', 'HomeController@index');
 
 Route::get('/submit', function () {
     return view('submit');
+});
+
+Route::post('/submit', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|max:255',
+        'url' => 'required|max:255',
+        'description' => 'required|max:255'
+    ]);
+
+    if ($validator->fails()) {
+        return back()->withInput()->withErrors($validator);
+    }
 });
