0aa06f4009ceb032d24eda0e0a440aae471fe0bf
root <napestershine@gmail.com>
2016-11-03 07:56:45 +0530

    fixed function name

--- tests/features/LinkTest.php
+++ tests/features/LinkTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LinkTest extends TestCase
 {
-    public function testLinksTable()
+    public function testWeSeeAListOfLinks()
     {
         factory(App\Link::class)->create([
             'title' => 'dotdev.co'
